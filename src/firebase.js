14f9166bbf96d4e9569db415f0e047d64ba3a3c7
Yohanes Demissie <Yohanesdemissie@gmail.com>
2019-02-08 17:11:30 -0800

    fire

---
+++ src/firebase.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+class ReactStuff extends React.Component {
+  render() {
+    return(
+      <h1>Hopefully this doesn't show</h1>
+    )
+  }
+}
+
+export default ReactStuff
