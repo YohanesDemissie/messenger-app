0504d4fb122a71aa17f547280f724d835a52d9e4
Yohanes Demissie <Yohanesdemissie@gmail.com>
2019-02-13 20:38:52 -0800

    home rendering on app load successfully

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import React, { Component, Fragment } from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
-
-import Home from './components/Home';
-import Login from './components/Auth/Login.js';
-import Register from './components/Auth/Register.js';
-import "semantic-ui-css/semantic.min.css"; //lets change this later to raw styling
-import './styles/App.css';
-
+import React, { Component } from 'react'
 
 class App extends Component {
   render() {
     return (
-      <Router>
-        <Fragment>
-          <Route exact path='/' component={Home}/>
-          <Route exact path='/Login' component={Login} />
-          <Route exact path='/Register' component={Register} />
-        </Fragment>
-      </Router>
-    );
+      <div>
+        <h1>App</h1>
+      </div>
+    )
   }
 }
 
 export default App;
+
+// import React, { Component, Fragment } from 'react';
+// import { BrowserRouter as Router, Route, withRouter } from 'react-router-dom';
+// import firebase from './firebase';
+
+// import Home from './components/Home';
+// import Login from './components/Auth/Login.js';
+// import Register from './components/Auth/Register.js';
+// import "semantic-ui-css/semantic.min.css"; //lets change this later to raw styling
+// import './styles/App.css';
+
+
+// class App extends Component {
+//   componentDidMount() {
+//     firebase.auth().onAuthStateChanged( user => {
+//       if (user) {
+//         this.props.history.push('/'); //redirects user to homeroute if firebause detects authorized user (aka when user logs in)
+//       }
+//     })
+//   }
+//   render() {
+//     return (
+//       <Router>
+//         <Fragment>
+//           <Route exact path='/' component={Home}/>
+//           <Route exact path='/Login' component={Login} />
+//           <Route exact path='/Register' component={Register} />
+//         </Fragment>
+//       </Router>
+//     );
+//   }
+// }
+
+// // const RootWithAuth = withRouter(App)
+
+// export default App;
--- src/index.js
+++ src/index.js
@@ -1,5 +1,39 @@
-import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import React, { Component, Fragment } from 'react';
+import { BrowserRouter as Router, Route, withRouter } from 'react-router-dom';
+import firebase from './firebase';
+
+import Home from './components/Home';
+import Login from './components/Auth/Login.js';
+import Register from './components/Auth/Register.js';
+import "semantic-ui-css/semantic.min.css"; //lets change this later to raw styling
+import './styles/App.css';
+
+
+class Root extends Component {
+  componentDidMount() {
+    firebase.auth().onAuthStateChanged(user => {
+      if (user) {
+        this.props.history.push('/'); //redirects user to homeroute if firebause detects authorized user (aka when user logs in)
+      }
+    })
+  }
+  render() {
+    return (
+        <Fragment>
+          <Route exact path='/' component={Home} />
+          <Route exact path='/Login' component={Login} />
+          <Route exact path='/Register' component={Register} />
+        </Fragment>
+    );
+  }
+}
+
+const RootWithAuth = withRouter(Root)
+
+ReactDOM.render(<Router><RootWithAuth /></Router>, document.getElementById('root'));
+
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import App from './App';
