fbe33f3189634df643b050c8cba9b098cad58be5
Yohanes Demissie <Yohanesdemissie@gmail.com>
2019-03-02 01:20:16 -0800

    added color picker modal on the side. Next add aditional functionality

--- src/App.js
+++ src/App.js
@@ -8,9 +8,6 @@ import SidePanel from './components/SidePanel/SidePanel';
 import Messages from './components/Message/Messages';
 import MetaPanel from './components/MetaPanel/MetaPanel';
 
-
-
-
 const App = ({ currentUser, currentChannel, isPrivateChannel, userPosts }) => (
       <Grid columns='equal' className='app' style={{ background: '#eee'}}>
         <ColorPanel />
@@ -46,39 +43,3 @@ const mapStateToProps = state => ({
 });
 
 export default connect(mapStateToProps)(App);
-
-// import React, { Component, Fragment } from 'react';
-// import { BrowserRouter as Router, Route, withRouter } from 'react-router-dom';
-// import firebase from './firebase';
-
-// import Home from './components/Home';
-// import Login from './components/Auth/Login.js';
-// import Register from './components/Auth/Register.js';
-// import "semantic-ui-css/semantic.min.css"; //lets change this later to raw styling
-// import './styles/App.css';
-
-
-// class App extends Component {
-//   componentDidMount() {
-//     firebase.auth().onAuthStateChanged( user => {
-//       if (user) {
-//         this.props.history.push('/'); //redirects user to homeroute if firebause detects authorized user (aka when user logs in)
-//       }
-//     })
-//   }
-//   render() {
-//     return (
-//       <Router>
-//         <Fragment>
-//           <Route exact path='/' component={Home}/>
-//           <Route exact path='/Login' component={Login} />
-//           <Route exact path='/Register' component={Register} />
-//         </Fragment>
-//       </Router>
-//     );
-//   }
-// }
-
-// // const RootWithAuth = withRouter(App)
-
-// export default App;
--- src/components/ColorPanel/ColorPanel.js
+++ src/components/ColorPanel/ColorPanel.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
-import { Sidebar, Menu, Divider, Button, Label } from 'semantic-ui-react'
+import { Sidebar, Menu, Divider, Button, Label, Modal, Icon, Segment } from 'semantic-ui-react';
+//import { StarterPicker } from 'react-color'; //REALLY COOL CUSTOM THEME MAKER UI FOR THE USER!!! APPLIED IN MODEM.CONTENT TAGS BELOW
+import { HuePicker } from 'react-color';
 
 class ColorPanel extends Component {
   state = {
     modal: false,
-  }
+  };
 
   openModal = () => this.setState({ modal: true});
 
@@ -12,18 +14,40 @@ class ColorPanel extends Component {
 
 
   render() {
+    const { modal } = this.state;
     return(
       <Sidebar
         as={Menu}
         icon="labeled"
         inverted
+        vertical
         visible
         width="very thin"
-        // ColorPanel
       >
-      <Divider />
-      <Button icon="add" size="small" color="blue" onClick={this.openModal} />
-      {/* color picker Modal*/}
+        <Divider />
+        <Button icon="add" size="small" color="blue" onClick={this.openModal} />
+
+        <Modal basic open={modal} onClose={this.closeModal}>
+          <Modal.Header>Change Color Theme</Modal.Header>
+          <Modal.Content>
+            <Segment>
+              <Label content="Primary Color" />
+              <HuePicker />
+            </Segment>
+            <Segment>
+              <Label content="Secondary Color" />
+              <HuePicker />
+            </Segment>
+          </Modal.Content>
+          <Modal.Actions>
+            <Button color="green" inverted>
+              <Icon name="checkmark" />Save Colors
+            </Button>
+            <Button color="red" inverted onClick={this.closeModal}>
+              <Icon name="remove" />cancel
+            </Button>
+          </Modal.Actions>
+        </Modal>
       </Sidebar>
     )
   }
